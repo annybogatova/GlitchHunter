e358a33fb820fb07627310e9836f6fc746622f45
Anna <annybogatova@gmail.com>
2025-11-13 18:24:32 +0300

    Enemy Navigation
    robot model and animation

---
+++ Assets/Scripts/Enemy/Navigation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour
+{
+    public Transform character;
+    private NavMeshAgent _agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.SetDestination(character.position);
+    }
+}
